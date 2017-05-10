b74c95a96d3d9a4c5e0e05e36b15b3cb0074746b
Simon Chen <Simonchen718@gmail.com>
2017-05-10 10:11:23 -0400

    adding code

---
+++ project-pocketchef/app/controllers/profiles_controller.rb
@@ -0,0 +1,27 @@
+class ProfilesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+  @profile = Profile.new
+  end
+
+  def create
+  @profile = Profile.new(params_profile)
+  
+  if @profile.save
+    redirect_to profile_path(@profile.id)
+  else
+    render 'new'
+  end
+  end
+
+
+  private
+
+  def params_profile
+    params.require(:profile).permit(:name, :preference)
+  end
+end
