a2e81b61d1acd1f1bee82fb1954407a785f88bc6
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-05-10 23:24:38 -0400

    api calls displaying to recipes/index

--- project-pocketchef/app/controllers/recipes_controller.rb
+++ project-pocketchef/app/controllers/recipes_controller.rb
@@ -1,2 +1,24 @@
 class RecipesController < ApplicationController
+
+  def index
+      @apiPath = 'https://api.edamam.com/search?q='
+      @appId = ENV['RECIPE_APP_ID']
+      @apiKey = ENV['RECIPE_API_KEY']
+
+      @recipe = params[:search]
+      @response = HTTParty.get(@apiPath.to_s + "" + @recipe.to_s  + ""+ @appId.to_s + "" + @apiKey.to_s)
+      puts "This is the recipe #{@recipe}"
+      @recipeResponse = JSON.parse(@response.body)
+      puts @recipeResponse["hits"][0]["recipe"]["label"]
+  end
+
+  def create
+    # @recipe = params[:search]
+    # @apiPath = 'https://api.edamam.com/search?q='
+    # @appId = ENV['RECIPE_APP_ID']
+    # @apiKey = ENV['RECIPE_API_KEY']
+    # @response = HTTParty.get(@apiPath.to_s + "" + @recipe.to_s  + ""+ @appId.to_s + "" + @apiKey.to_s)
+    # puts "Recipe is #{@recipe}"
+    redirect_to recipes_path(:search => params[:search])
+  end
 end
--- project-pocketchef/db/schema.rb
+++ project-pocketchef/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509192557) do
+ActiveRecord::Schema.define(version: 20170510194054) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170509192557) do
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "search"
   end
 
   create_table "users", force: :cascade do |t|
