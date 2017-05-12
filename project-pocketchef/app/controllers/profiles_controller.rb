cdf42e27d702e90cde76f54926d959936e663dd5
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-05-12 05:06:18 -0400

    more styling

--- project-pocketchef/app/controllers/profiles_controller.rb
+++ project-pocketchef/app/controllers/profiles_controller.rb
@@ -7,25 +7,25 @@ class ProfilesController < ApplicationController
   end
 
   def create
-  @profile = Profile.new(params_profile)
-  @profile.user_id = current_user.id
-    if @profile.save
-      redirect_to profile_path(@profile.user_id)
-    else
-      render 'new'
-    end
+    @profile = Profile.new(params_profile)
+    @profile.user_id = current_user.id
+      if @profile.save
+        redirect_to profile_path(@profile.user_id)
+      else
+        render 'new'
+      end
   end
 
   def show
-   @profile = Profile.find(params[:id])
-   @recipe = Recipe.all
-   @apiPath = 'https://api.edamam.com/search?q='
-   @appId = ENV['RECIPE_APP_ID']
-   @apiKey = ENV['RECIPE_API_KEY']
+     @profile = Profile.find(params[:id])
+     @recipe = Recipe.all
+     @apiPath = 'https://api.edamam.com/search?q='
+     @appId = ENV['RECIPE_APP_ID']
+     @apiKey = ENV['RECIPE_API_KEY']
 
-   @response = HTTParty.get(@apiPath.to_s + "" + @recipe_pref.to_s  + ""+ @appId.to_s + "" + @apiKey.to_s + "&from=0" + "&to=5")
-   @recipeResponse = JSON.parse(@response.body)
-   @recipe_pref = params[:search] || @profile.preference
+     @response = HTTParty.get(@apiPath.to_s + "" + @recipe_pref.to_s  + ""+ @appId.to_s + "" + @apiKey.to_s + "&from=0" + "&to=5")
+     @recipeResponse = JSON.parse(@response.body)
+     @recipe_pref = params[:search]
   end
 
 
